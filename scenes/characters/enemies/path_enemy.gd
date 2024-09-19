2a29068510962b366ef507177ec83a291033690c
Brad Hendrickson <bradley.hendrickson6@gmail.com>
2024-09-18 22:06:20 -0700

    add enemy hit animation

--- scenes/characters/enemies/path_enemy.gd
+++ scenes/characters/enemies/path_enemy.gd
@@ -5,6 +5,7 @@ extends CharacterBody2D
 @onready var ray = $"RayCast2D"
 @export var INITIAL_ANGLE = 0.0 # in degrees
 @export var FORCE = 50
+@onready var sprite_animation_player: AnimationPlayer = $AnimatedSprite2D/SpriteAnimationPlayer
 
 @export var move_to := Vector2(0, -128): 
 	set(value):
@@ -13,7 +14,7 @@ extends CharacterBody2D
 	get():
 		return move_to
 		
-@export var health: int = 2: 
+@export var health: int = 3: 
 	set(value):
 		health = value
 		if health <= 0:
@@ -23,10 +24,13 @@ extends CharacterBody2D
 
 
 func _ready() -> void:
+	setShader(false)
 	var radAngle = INITIAL_ANGLE * PI / 180
 	ray.global_rotation = radAngle
 	velocity = Vector2(FORCE * cos(radAngle), FORCE * sin(radAngle))
 
+func setShader(value):
+	animated_sprite_2d.material.set("shader_param/active", value);
 
 func _process(delta: float) -> void:
 	
@@ -43,4 +47,5 @@ func _process(delta: float) -> void:
 		move_and_slide()
 
 func hit(dmgTaken):
+	sprite_animation_player.play("hit")
 	health -= dmgTaken
