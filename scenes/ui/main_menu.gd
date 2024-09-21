b362b7b43b301cb6a65252239002298b20e2e90d
slaker333 <slaker333@gmail.com>
2024-09-21 12:26:28 -0700

    Added play button to main menu and made main menu the start scene.

--- scenes/ui/main_menu.gd
+++ scenes/ui/main_menu.gd
@@ -12,6 +12,9 @@ func _process(delta: float) -> void:
 		get_tree().quit()
 
 
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level/level_1.tscn")
+
+
 func _on_settings_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/ui/settings_menu.tscn")
-	pass # Replace with function body.
