8655b26c0fc971b8b6c8a2051f23f746c1c885cb
Samuel LaBrant <sjlabrant@gmail.com>
2024-09-22 00:38:05 -0500

    Stitch together start of game.

---
+++ scenes/level/level_start.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+@onready var closed_door: TileMapLayer = $ClosedDoor
+@onready var open_door: TileMapLayer = $OpenDoor
+@onready var timer: Timer = $Timer
+
+var time_left = 0.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	open_door.modulate.a = 1.0
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if !timer.is_stopped():
+		print(time_left - timer.time_left)
+		if 0.01 < time_left - timer.time_left:
+			open_door.modulate.a -= 0.05
+			time_left = timer.time_left
+
+
+func _on_area_2d_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	if timer.is_stopped():
+		timer.start(1)
+		time_left = timer.time_left
+
+
+func _on_next_level_area_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	get_tree().change_scene_to_file("res://scenes/level/level_main.tscn")
